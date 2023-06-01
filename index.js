fb35c88fe527fc585b58944da44e0b2fbd513e77
francatania <francocatania93@gmail.com>
2023-05-31 21:03:22 -0300

    Se modificó la funcion verPrecios() para poder calcular cuotas correctamente en la funcion calcularCuotas()

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 let username = ""
 let usuarioLogueado = false
-const autoss = "Peugeot 208, FIAT Cronos, VW Gol, Toyota Corolla"
-const precio208 = 7500000
-const precioCronos = 5000000
-const precioGol = 6500000
-const precioCorolla = 9000000
 
 class Autos {
     constructor(id, marca, nombre, precio, stock) {
@@ -25,8 +20,8 @@ const autos = [{ id: 1327, marca: "Peugeot", nombre: "208", precio: 7500000, sto
 { id: 1327, marca: "VW", nombre: "Golf", precio: 8300000, stock: 15 }]
 
 function login() {
-    username = prompt("Ingresa tu usuario.")
-    password = prompt("Ingresa tu contraseña.")
+    username = prompt("Ingresa tu usuario. ('admin' para tener todas las funciones)")
+    password = prompt("Ingresa tu contraseña. ('admin' para tener todas las funciones)")
     if (username.trim() !== "" && password.trim() !== "") {
         alert("✅ BIENVENID@ " + username)
         usuarioLogueado = true
@@ -38,10 +33,11 @@ function login() {
 
 function verAutos() {
     autos.forEach((auto) => console.log(auto.marca + " " + auto.nombre + " " + "$" + auto.precio + " " + auto.stock))
+    console.log(" ")
 }
 
 function agregarAutos() {
-    let id = Math.random * 1000
+    let id = Math.random * 10000
     let marca = prompt("Marca: ")
     let nombre = prompt("Nombre: ")
     let precio = parseFloat(prompt("Precio: "))
@@ -66,45 +62,35 @@ function agregarStock() {
 }
 
 function verPrecios() {
-    eleccion = prompt(("Elija el auto: 1 para 'Peugeot 208', 2 para 'FIAT Cronos', 3 para 'VW Gol' y 4 para 'Toyota Corolla'"))
-    switch (eleccion) {
-        case "1":
-            alert("El precio es de $7500000")
-            return precio208
-        case "2":
-            alert("El precio es de $5000000")
-            return precioCronos
-        case "3":
-            alert("El precio es de $6500000")
-            return precioGol
-        case "4":
-            alert("El precio es de $9000000")
-            return precioCorolla
-        default:
-            alert("❌Error. Elegir entre el 1 y 4.")
-            break
+    for (let auto of autos) {
+        console.log(auto.nombre + " " + "$" + auto.precio)
     }
+    console.log(" ")
+    let buscador = prompt("Escribí el auto que querés: ")
+    let eleccion = buscador.toUpperCase()
+    let buscadorPrecio = autos.find(auto => auto.nombre.toUpperCase() === eleccion)
+    return buscadorPrecio.precio
 }
 
 function calcularCuotas(cuotas, precio) {
-    let precioCuota
+    let precioCuota = 0
     let interes1 = 1.25
     let interes2 = 1.50
     if (cuotas < 25) {
         precioCuota = (precio / cuotas)
         for (let i = 0; i < cuotas; i++) {
-            console.log("cuota " + (i + 1) + ":" + precioCuota)
+            console.log("cuota " + (i + 1) + ":" + " " + "$" + precioCuota.toFixed(0))
         }
     }
     else if (cuotas < 37 && cuotas > 24) {
         for (let i = 0; i < cuotas; i++) {
             if (i < 24) {
                 precioCuota = (precio / cuotas)
-                console.log("cuota " + (i + 1) + ":" + precioCuota)
+                console.log("cuota " + (i + 1) + ":" + " " + "$" + precioCuota.toFixed(0))
             }
             else {
                 precioCuota = (precio * interes1) / cuotas
-                console.log("cuota " + (i + 1) + ":" + precioCuota)
+                console.log("cuota " + (i + 1) + ":" + " " + "$" + precioCuota.toFixed(0))
             }
         }
     }
@@ -112,15 +98,15 @@ function calcularCuotas(cuotas, precio) {
         for (let i = 0; i < cuotas; i++) {
             if (i < 24) {
                 precioCuota = (precio / cuotas)
-                console.log("cuota " + (i + 1) + ":" + precioCuota)
+                console.log("cuota " + (i + 1) + ":" + " " + "$" + precioCuota.toFixed(0))
             }
             else if (i >= 24 && i < 37) {
                 precioCuota = (precio * interes1) / cuotas
-                console.log("cuota " + (i + 1) + ":" + precioCuota)
+                console.log("cuota " + (i + 1) + ":" + " " + "$" + precioCuota.toFixed(0))
             }
             else {
                 precioCuota = (precio * interes2) / cuotas
-                console.log("cuota " + (i + 1) + ":" + precioCuota)
+                console.log("cuota " + (i + 1) + ":" + " " + "$" + precioCuota.toFixed(0))
             }
         }
 
@@ -131,7 +117,7 @@ function abrirMenu() {
     login()
     while (usuarioLogueado) {
         let opcion = 0
-        opcion = parseInt(prompt("Que desea hacer? Presione '1' para ver nuestro catalogo de autos, '2' para calcular cuotas, '3' para agregar autos (ADMIN) y '4' para agregar stock (admin) y '5 para salir"))
+        opcion = parseInt(prompt("Que desea hacer? Presione '1' para ver nuestro catalogo de autos, '2' para calcular cuotas, '3' para agregar autos (admin) y '4' para agregar stock (admin) y '5 para salir"))
         if (opcion === 1) {
             verAutos()
         }
@@ -141,11 +127,21 @@ function abrirMenu() {
             cantidadCuotas = parseInt(prompt("Cuantas cuotas queres? Hasta 💥24 cuotas SIN INTERES💥. A partir de la cuota 25 hay un %25 interes y a partir de la cuota 37 un %50"))
             calcularCuotas(cantidadCuotas, precio)
         }
-        else if (opcion === 3 && username === "admin") {
-            agregarAutos()
+        else if (opcion === 3) {
+            if (username === "admin") {
+                agregarAutos()
+            }
+            else {
+                alert("Usted no es admin.")
+            }
         }
-        else if (opcion === 4 && username === "admin") {
-            agregarStock()
+        else if (opcion === 4) {
+            if (username === "admin") {
+                agregarStock()
+            }
+            else {
+                alert("Usted no es admin.")
+            }
         }
         else if (opcion === 5) {
             alert("Adios!!")
